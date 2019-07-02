171bbe533b911fbc1eaf3e5b99a6976db0f8527d
NormalZhou <15221641446@163.com>
2019-07-02 17:57:15 +0800

    折扣劵常量定义

--- src/main/java/com/coding/business/constant/Discount.java
+++ src/main/java/com/coding/business/constant/Discount.java
@@ -4,6 +4,6 @@ package com.coding.business.constant;
  * 折扣劵常量定义
  */
 public class Discount {
-    private static final double DISCOUNTOFNINE = 0.9;//九折
-    private static final double DISCOUNTOFNINEFIVE = 0.95;//九五折
+    public static final double DISCOUNTOFNINE = 0.9;//九折
+    public static final double DISCOUNTOFNINEFIVE = 0.95;//九五折
 }
