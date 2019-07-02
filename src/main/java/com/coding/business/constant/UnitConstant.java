18f4bcd5ef05c22a558b38dd2450939830d516c4
NormalZhou <15221641446@163.com>
2019-07-02 19:35:45 +0800

    单位常量类定义

---
+++ src/main/java/com/coding/business/constant/UnitConstant.java
@@ -0,0 +1,15 @@
+package com.coding.business.constant;
+
+/**
+ * 单位常量类
+ */
+public class UnitConstant {
+    /** 册 */
+    public static final String BOOK = "册";
+    /** 盒 */
+    public static final String BOX = "盒";
+    /** 条 */
+    public static final String ITEM = "条";
+    /** 套 */
+    public static final String SUIT = "套";
+}
