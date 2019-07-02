cdb443e354aeeb9db4411995a7a9c29df86cbe8d
yunlongli32 <yunlongli32@163.com>
2019-07-02 16:57:56 +0800

    客户等级枚举定义

---
+++ src/main/java/com/coding/business/constant/CustomerGrade.java
@@ -0,0 +1,15 @@
+package com.coding.business.constant;
+
+/**
+ * 客户等级枚举类
+ */
+public enum CustomerGrade {
+    /** 普卡 */
+    NORMAL,
+    /** 金卡 */
+    GOLDEN,
+    /** 白金卡 */
+    PLATINUM,
+    /** 钻石卡 */
+    DIAMOND
+}
