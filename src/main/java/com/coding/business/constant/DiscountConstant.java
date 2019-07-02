ab6121fb5a3708949a4e4aac760b43006d0f8a95
NormalZhou <15221641446@163.com>
2019-07-02 19:24:29 +0800

    满减常量类定义

---
+++ src/main/java/com/coding/business/constant/DiscountConstant.java
@@ -0,0 +1,15 @@
+package com.coding.business.constant;
+
+/**
+ * 折扣劵常量定义
+ */
+public class DiscountConstant {
+    /**
+     * 九折
+     */
+    public static final double DISCOUNTOFNINE = 0.9;
+    /**
+     * 九五折
+     */
+    public static final double DISCOUNTOFNINEFIVE = 0.95;
+}
