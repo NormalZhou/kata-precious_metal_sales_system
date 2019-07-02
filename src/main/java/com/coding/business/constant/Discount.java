a7d429a3d8ce625078ad968fecaaf08f2482f8a8
NormalZhou <15221641446@163.com>
2019-07-02 19:29:42 +0800

    单位枚举删除

--- src/main/java/com/coding/business/constant/Discount.java
+++
@@ -1,9 +0,0 @@
-package com.coding.business.constant;
-
-/**
- * 折扣劵常量定义
- */
-public class Discount {
-    public static final double DISCOUNTOFNINE = 0.9;//九折
-    public static final double DISCOUNTOFNINEFIVE = 0.95;//九五折
-}
